c29f165e455aacb3c89a5bdd0f7eddf3a5e62cbc
Srikanth <srikanth9000093071@gmail.com>
2017-03-22 19:14:54 +0530

    Hello srikanth

--- HelloWorld/src/info/cds/HelloWorld.java
+++ HelloWorld/src/info/cds/HelloWorld.java
@@ -4,14 +4,10 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-
 		//Iam srikanth
 		int a=10;
 		System.out.println("Hello welcome to git bye kanth...");
 		//comment
-		
-		
-		
 	}
 
 }
