b3386eb25219e94756b5cb73ff53f8f4a979ed93
Srikanth <srikanth9000093071@gmail.com>
2017-03-22 19:06:35 +0530

    This sysout diff

--- HelloWorld/src/info/cds/HelloWorld.java
+++ HelloWorld/src/info/cds/HelloWorld.java
@@ -3,11 +3,13 @@ package info.cds;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello welcome to git...");
+		System.out.println("Hello welcome to git bye kanth...");
 		//comment
 		
 		//Iam srikanth
 		int a=10;
+		
+		
 	}
 
 }
