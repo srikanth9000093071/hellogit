144d3551dee1cc9c4bdb2c96d4ff9f1f8e833656
Srikanth <srikanth9000093071@gmail.com>
2017-03-22 19:12:48 +0530

    ok i am accepted

--- HelloWorld/src/info/cds/HelloWorld.java
+++ HelloWorld/src/info/cds/HelloWorld.java
@@ -3,11 +3,13 @@ package info.cds;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
+		
+
+		//Iam srikanth
+		int a=10;
 		System.out.println("Hello welcome to git bye kanth...");
 		//comment
 		
-		//Iam srikanth
-		int a=10;
 		
 		
 	}
